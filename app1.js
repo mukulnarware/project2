9d77e037be384691f215d189391e1a27489d7974
mukulnarware <narwaremukul1997@gmail.com>
2020-08-16 23:00:34 +0530

    commmiting for project 2

---
+++ app1.js
@@ -0,0 +1,85 @@
+// Express Imports
+
+const myData = require("./public/data5");
+
+const express = require("express")
+const app = express();
+const port = process.env.PORT || 3500;
+
+
+
+app.use(express.static("public/"))
+// app.use(express.static("public/app.js"));
+// app.use(express.static("public/index.html"));
+
+// app.use(express.static("public/styles.css"));
+
+// app.use(express.static("../ipl/economicBowler.js"));
+// app.use(express.static("../ipl/extraRunByTeam.js"));
+// app.use(express.static("../ipl/matchesPlayedByCity.js"));
+// app.use(express.static("../ipl/matchesPlayedPerYear.js"));
+// app.use(express.static("../ipl/matchesWonByEachTeam.js"));
+// app.use(express.static("../ipl/yearWiseBowler.js"));
+// app.use(express.static("/index.js"));
+
+
+
+// app.get("/" ,(req, res) => {
+
+//   res.sendFile(__dirname + "/index.html");
+  
+// });
+
+// app.get( (req, res) => {
+
+//   res.sendFile(__dirname + "/styles.css");
+
+// });
+
+// app.get( (req, res) => {
+
+//   res.sendFile(__dirname + "/app.js");
+  
+// });
+
+// app.get((req, res) => {
+
+//   res.sendFile(__dirname + "/app1.js");
+  
+// });
+
+
+// app.get( (req, res) => {
+
+//   res.sendFile("../index.js");
+//   return
+// });
+
+// app.get('/api', (req, res) => {
+  //res.send(req.query.YEAR);
+  // res.sendFile(__dirname+"/data3.json");
+  // const year = req.query.YEAR
+
+  //  var a = {};
+  // for (let x in myData) {
+  //   a[x] = myData[x];
+
+  // }
+
+  // const seriesData = [];
+  // for (let name in a[year]) {
+  //   seriesData.push([name, a[year][name]]);
+  // }
+  // a[year]=myData[year];
+  // console.log(a);
+  // res.send(a)
+
+  // return
+
+// });
+
+
+app.listen(port);
+
+
+
